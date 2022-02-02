8fe9312e0cbb6d0c3409b188031d645ca1129256
yurley <yurs.ksf1@gmail.com>
2022-02-02 18:51:54 -0500

    solution calculadora

--- session_2/Ejercicio_2/script.js
+++ session_2/Ejercicio_2/script.js
@@ -1,4 +1,28 @@
+// aca esta la suma 
+
 let number_1 = prompt("Ingresa el primer numero:", 30);
 let number_2 = prompt("Ingresa el segundo numero:", 60);
-const resulatado = parseInt(number_1) + parseInt(number_2);
+let resulatado = parseInt(number_1) + parseInt(number_2);
 alert('la suma de ' + number_1 + ' + ' + number_2 + ' = ' + resulatado);
+
+// aca esta la resta 
+
+number_1 = prompt("Ingresa el primer numero:", 30);
+number_2 = prompt("Ingresa el segundo numero:", 60);
+resulatado = parseInt(number_1) - parseInt(number_2);
+alert('la resta de ' + number_1 + ' - ' + number_2 + ' = ' + resulatado);
+
+// aca esta la multiplicacion 
+
+number_1 = prompt("Ingresa el primer numero:", 30);
+number_2 = prompt("Ingresa el segundo numero:", 60);
+resulatado = parseInt(number_1) * parseInt(number_2);
+alert('la multiplicacion de ' + number_1 + ' * ' + number_2 + ' = ' + resulatado);
+
+// aca esta la division  
+
+number_1 = prompt("Ingresa el primer numero:", 30);
+number_2 = prompt("Ingresa el segundo numero:", 60);
+resulatado = parseInt(number_1) / parseInt(number_2);
+alert('la division de ' + number_1 + ' / ' + number_2 + ' = ' + resulatado);
+
