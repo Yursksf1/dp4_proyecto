6d15a24f85c77033e0596cbd4b2267ca50b28ffe
yurley <yurs.ksf1@gmail.com>
2022-02-02 19:12:54 -0500

    ejecricio calculadora - botones

--- session_2/Ejercicio_2/script.js
+++ session_2/Ejercicio_2/script.js
@@ -1,28 +1,47 @@
-// aca esta la suma 
 
-let number_1 = prompt("Ingresa el primer numero:", 30);
-let number_2 = prompt("Ingresa el segundo numero:", 60);
-let resulatado = parseInt(number_1) + parseInt(number_2);
-alert('la suma de ' + number_1 + ' + ' + number_2 + ' = ' + resulatado);
+function run() {
+    console.log('Estoy corriendo el metodo run')
+    // aca esta la suma 
 
-// aca esta la resta 
+    // let number_1 = prompt("Ingresa el primer numero:", 30);
+    // let number_2 = prompt("Ingresa el segundo numero:", 60);
+    // let resulatado = parseInt(number_1) + parseInt(number_2);
+    // alert('la suma de ' + number_1 + ' + ' + number_2 + ' = ' + resulatado);
 
-number_1 = prompt("Ingresa el primer numero:", 30);
-number_2 = prompt("Ingresa el segundo numero:", 60);
-resulatado = parseInt(number_1) - parseInt(number_2);
-alert('la resta de ' + number_1 + ' - ' + number_2 + ' = ' + resulatado);
+    // // aca esta la resta 
 
-// aca esta la multiplicacion 
+    // number_1 = prompt("Ingresa el primer numero:", 30);
+    // number_2 = prompt("Ingresa el segundo numero:", 60);
+    // resulatado = parseInt(number_1) - parseInt(number_2);
+    // alert('la resta de ' + number_1 + ' - ' + number_2 + ' = ' + resulatado);
 
-number_1 = prompt("Ingresa el primer numero:", 30);
-number_2 = prompt("Ingresa el segundo numero:", 60);
-resulatado = parseInt(number_1) * parseInt(number_2);
-alert('la multiplicacion de ' + number_1 + ' * ' + number_2 + ' = ' + resulatado);
+    // // aca esta la multiplicacion 
 
-// aca esta la division  
+    // number_1 = prompt("Ingresa el primer numero:", 30);
+    // number_2 = prompt("Ingresa el segundo numero:", 60);
+    // resulatado = parseInt(number_1) * parseInt(number_2);
+    // alert('la multiplicacion de ' + number_1 + ' * ' + number_2 + ' = ' + resulatado);
 
-number_1 = prompt("Ingresa el primer numero:", 30);
-number_2 = prompt("Ingresa el segundo numero:", 60);
-resulatado = parseInt(number_1) / parseInt(number_2);
-alert('la division de ' + number_1 + ' / ' + number_2 + ' = ' + resulatado);
+    // // aca esta la division  
 
+    // number_1 = prompt("Ingresa el primer numero:", 30);
+    // number_2 = prompt("Ingresa el segundo numero:", 60);
+    // resulatado = parseInt(number_1) / parseInt(number_2);
+    // alert('la division de ' + number_1 + ' / ' + number_2 + ' = ' + resulatado);
+
+}
+
+function run_1() { 
+    console.log('estoy corriendo la funcion run_1')
+    // aca esta la suma 
+
+    let number_1 = prompt("Ingresa el primer numero:", 30);
+    let number_2 = prompt("Ingresa el segundo numero:", 60);
+    let resulatado = parseInt(number_1) + parseInt(number_2);
+    alert('la suma de ' + number_1 + ' + ' + number_2 + ' = ' + resulatado);
+
+}
+
+function run_2() { console.log('estoy corriendo la funcion run_2')}
+function run_3() { console.log('estoy corriendo la funcion run_3')}
+function run_4() { console.log('estoy corriendo la funcion run_4')}
