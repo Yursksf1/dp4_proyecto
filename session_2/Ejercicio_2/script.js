55bf5ce079882d39a286381a42c33c20e111924f
yurley <yurs.ksf1@gmail.com>
2022-02-04 19:38:37 -0500

    use jquery in the proyect

--- session_2/Ejercicio_2/script.js
+++ session_2/Ejercicio_2/script.js
@@ -1,37 +1,31 @@
-window.onload = function() {
-    document.getElementById("btn_run_1").addEventListener(
-        "click", run_1
-    )
-    document.getElementById("btn_run_2").addEventListener(
-        "click", run_2
-    )
-    document.getElementById("btn_run_3").addEventListener(
-        "click", run_3
-    )
-    document.getElementById("btn_run_4").addEventListener(
-        "click", run_4
-    )
-}
 
+$(document).ready(function(){
+    $("#btn_run_1").click(run_1);
+    $("#btn_run_2").click(run_2);
+    $("#btn_run_3").click(run_3);
+    $("#btn_run_4").click(run_4);
+});
 
 
 function run_1() { 
     // aca esta la suma 
 
-    let number_1 = document.getElementById("number_1").value;
-    let number_2 = document.getElementById("number_2").value;
+    let number_1 = $("#number_1")[0].value;
+    let number_2 = $("#number_2")[0].value;
     let resulatado = parseInt(number_1) + parseInt(number_2);
-    document.getElementById('resultado').textContent = resulatado
+    $('#resultado')[0].textContent = resulatado
+
     // alert('la suma de ' + number_1 + ' + ' + number_2 + ' = ' + resulatado);
 }
 
 function run_2() { 
     // aca esta la suma 
 
-    let number_1 = document.getElementById("number_1").value;
-    let number_2 = document.getElementById("number_2").value;
+    let number_1 = $("#number_1")[0].value;
+    let number_2 = $("#number_2")[0].value;
     let resulatado = parseInt(number_1) - parseInt(number_2);
-    document.getElementById('resultado').textContent = resulatado
+    $('#resultado')[0].textContent = resulatado
+
 
     // alert('la resta de ' + number_1 + ' - ' + number_2 + ' = ' + resulatado);
 }
@@ -39,10 +33,11 @@ function run_2() {
 function run_3() { 
     // aca esta la suma 
 
-    let number_1 = document.getElementById("number_1").value;
-    let number_2 = document.getElementById("number_2").value;
+    let number_1 = $("#number_1")[0].value;
+    let number_2 = $("#number_2")[0].value;
     let resulatado = parseInt(number_1) * parseInt(number_2);
-    document.getElementById('resultado').textContent = resulatado
+    $('#resultado')[0].textContent = resulatado
+
 
     // alert('la multiplicacion de ' + number_1 + ' * ' + number_2 + ' = ' + resulatado);
 }
@@ -50,10 +45,10 @@ function run_3() {
 function run_4() { 
     // aca esta la suma 
 
-    let number_1 = document.getElementById("number_1").value;
-    let number_2 = document.getElementById("number_2").value;
+    let number_1 = $("#number_1")[0].value;
+    let number_2 = $("#number_2")[0].value;
     let resulatado = parseInt(number_1) / parseInt(number_2);
-    document.getElementById('resultado').textContent = resulatado
+    $('#resultado')[0].textContent = resulatado
 
     // alert('la division de ' + number_1 + ' / ' + number_2 + ' = ' + resulatado);
 }
