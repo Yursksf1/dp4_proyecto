55b065e04661f05a6be06a3b7a56e3c53e0a78d7
yurley <yurs.ksf1@gmail.com>
2022-02-21 19:25:50 -0500

    save previous taller

---
+++ session_6_python_rw_files/excel_read/test.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+import pandas as pd
+pd.__version__
+
+california_housing_dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+print(california_housing_dataframe.describe())
+
