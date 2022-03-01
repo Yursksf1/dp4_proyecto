a879d8d63e4130eec20a37733d6ca25bbf3d806f
yurley <yurs.ksf1@gmail.com>
2022-02-28 21:16:33 -0500

    solution actividad flask

--- taller_candidatos/candidatos.py
+++ taller_candidatos/candidatos.py
@@ -6,19 +6,51 @@ from flask import (
     request,
     render_template
 )
+import csv
+
+lista = []
+with open('candidatos.csv') as File:
+    reader = csv.reader(File)
+    for row in reader:
+        lista.append(row)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    candidatos = []
+    for elemento in lista:
+        candidatos.append(
+            {
+                "id": elemento[0],
+                "name": elemento[1]
+            }
+        )
+
+    return render_template("index.html", lista=candidatos)
 
 
-@app.route("/candidato/<name>")
-def get_candidate(name):
-    content = ""
-    return content
+@app.route("/candidato/<id>")
+def get_candidate(id):
+    candidato = None
+    nombre = "No se encontro el nombre"
+    descripcion = "No se encontro alguna descripcion"
+    for elemento in lista:
+        if elemento[0] == id:
+            candidato = elemento
+
+    if candidato:
+        nombre = candidato[1]
+        descripcion = candidato[2]
+
+
+    return render_template(
+        "candidato.html",
+        candidato=candidato,
+        nombre=nombre,
+        descripcion=descripcion
+    )
 
 if __name__ == '__main__':
    app.run()
