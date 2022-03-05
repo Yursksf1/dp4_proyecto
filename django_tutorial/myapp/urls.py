5972754773dae3477d98780dfe8ac03662dc2acb
yurley <yurs.ksf1@gmail.com>
2022-03-05 11:39:59 -0500

    documentation django

--- django_tutorial/myapp/urls.py
+++ django_tutorial/myapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('install', views.install, name='install'),
-    path('models', views.models, name='models'),
-    path('002', views.index3, name='chiste_2'),
+    path('archivos/models', views.models, name='models'),
+    path('archivos/urls', views.urls, name='urls'),
+    path('archivos/views', views.views, name='views'),
+    path('archivos/templates', views.templates, name='templates'),
 ]
--- django_tutorial/myapp/views.py
+++ django_tutorial/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,10 +7,21 @@ def index(request):
     return render(request, 'myapp/index.html', {})
 
 def install(request):
+    print('estoy llamado a install')
+
     return render(request, 'myapp/install.html', {})
 
 def models(request):
+    print('estoy llamado a models')
+    # return redirect('install')
     return render(request, 'myapp/models.html', {})
 
-def index3(request):
-    return render(request, 'myapp/index_2.html', {})
+def urls(request):
+    return render(request, 'myapp/urls.html', {})
+    
+def views(request):
+    return render(request, 'myapp/views.html', {})
+
+def templates(request):
+    return render(request, 'myapp/templates.html', {})
+
