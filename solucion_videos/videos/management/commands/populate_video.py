ab3f00b2ce7a22f53829577d49f7fa41c8dffe2d
ysanchez <yadasa55@gmail.com>
2022-03-12 11:26:16 -0500

    open excel

--- solucion_videos/videos/management/commands/populate_video.py
+++ solucion_videos/videos/management/commands/populate_video.py
@@ -8,6 +8,14 @@ from videos.models import Video
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print("vamos a hacer un populate es decir llenar la data de la tabla video")
+        import pandas as pd
+
+        path = r'C:\Users\yajhaira.sanchez\proyectos_yurley\dp4_proyecto\solucion_videos\videos\management\commands\import.xlsx'
+        df = pd.read_excel(path)
+        print("show df")
+        print(df)
+
+        
         video_data = ['nombre del video', 'descripcion del video', 'www.youtube.com']
         video = Video()
         video.title = video_data[0]
