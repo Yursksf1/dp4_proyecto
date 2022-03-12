482db2c3c30631d2612d5e237d7ad96f521b59b6
ysanchez <yadasa55@gmail.com>
2022-03-12 10:42:05 -0500

    sotre a record

--- solucion_videos/videos/management/commands/populate_video.py
+++ solucion_videos/videos/management/commands/populate_video.py
@@ -8,3 +8,9 @@ from videos.models import Video
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print("vamos a hacer un populate es decir llenar la data de la tabla video")
+        video_data = ['nombre del video', 'descripcion del video', 'www.youtube.com']
+        video = Video()
+        video.title = video_data[0]
+        video.description = video_data[1]
+        video.link = video_data[2]
+        video.save()
