3445288d8533d8b4f0459a78912ecaf49aebfafd
ysanchez <yadasa55@gmail.com>
2022-03-12 11:39:33 -0500

    populate all data form xlsx

--- solucion_videos/videos/management/commands/populate_video.py
+++ solucion_videos/videos/management/commands/populate_video.py
@@ -3,22 +3,23 @@ from django.core.management.base import BaseCommand
 
 # Models
 from videos.models import Video
+import pandas as pd
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print("vamos a hacer un populate es decir llenar la data de la tabla video")
-        import pandas as pd
-
         path = r'C:\Users\yajhaira.sanchez\proyectos_yurley\dp4_proyecto\solucion_videos\videos\management\commands\import.xlsx'
         df = pd.read_excel(path)
         print("show df")
         print(df)
-
         
-        video_data = ['nombre del video', 'descripcion del video', 'www.youtube.com']
-        video = Video()
-        video.title = video_data[0]
-        video.description = video_data[1]
-        video.link = video_data[2]
-        video.save()
+        for index, row in df.iterrows():
+            print("creando registro.")
+            video = Video()
+            video.title = row["titulo"]
+            video.description = row["descripcion"]
+            video.link = row["link"]
+            video.save()
+
+        print("fin")
